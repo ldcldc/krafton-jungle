8c16d40f1a752456356a860339879e724093e85c
ldcldc <1qw23er45ty08@gmail.com>
2026-08-27 21:29:05 +0900

    02

--- week2/1. basic/02_array.py
+++ week2/1. basic/02_array.py
@@ -42,15 +42,20 @@ def rotate_matrix_90(matrix):
         회전된 2차원 리스트
     """
     n = len(matrix)
-    
+    rotated = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            rotated[i][j] = matrix[i][j]
+
+    return rotated
+
     # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
     pass
         
     # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
     # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
     pass
-    
-    return rotated
 
 def print_matrix(matrix):
     """배열을 보기 좋게 출력하는 헬퍼 함수"""
